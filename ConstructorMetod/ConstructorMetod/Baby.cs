5c716d45cd89d8518e3d12b68970e97c087b89d4
yilmazcinar <cinaryilmaz96@gmail.com>
2025-01-15 18:58:23 +0300

    Add files via upload

---
+++ ConstructorMetod/ConstructorMetod/Baby.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorMetod
+{
+    internal class Baby
+    {
+        //Constructor metod.
+        public Baby()
+        {
+
+            Console.WriteLine("Ingaaaaa");
+            
+            Isim = "Daha isim koyulmadı.";
+
+            DogumTarihi = DateTime.Now;
+
+        }
+
+
+        public Baby(string isim, string soyIsim)
+        {
+            Isim = isim;
+            
+            Soyisim = soyIsim;
+            
+            Console.WriteLine("Ingaaaaa");
+
+            DogumTarihi = DateTime.Now;
+
+        }
+
+        public string Isim {  get; set; }
+
+        public string Soyisim {  get; set; }
+
+        public DateTime DogumTarihi { get; set; }
+
+
+    }
+}
---
+++ ConstructorMetod/ConstructorMetod/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using ConstructorMetod;
+
+Baby yenidogan = new Baby();
+
+Console.WriteLine($"Yeni doğan bilgisi :\n\nİsim = {yenidogan.Isim}\n\nSoyisim = {yenidogan.Soyisim}\n\nDoğum Tarihi = {yenidogan.DogumTarihi}");
+
+Baby yenidogan2 = new Baby("Yılmaz", "Çınar");
+
+Console.WriteLine($"Yeni doğan bilgisi :\n\nİsim = {yenidogan2.Isim}\n\nSoyisim = {yenidogan2.Soyisim}\n\nDoğum Tarihi = {yenidogan2.DogumTarihi}");
